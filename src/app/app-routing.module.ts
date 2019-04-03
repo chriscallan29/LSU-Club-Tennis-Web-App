72de0103f4b36589691fb7293e26a0b123b201ca
Christopher Callan <Christopher.Robert.Callan@ibm.com>
2019-04-03 17:07:54 -0500

    Cleaned up Routing Code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,8 +14,6 @@ const routes: Routes = [
   { path: 'support', component: SupportComponent},
 ];
 
-RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'});
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
