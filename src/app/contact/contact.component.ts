c09deea4874eb96aea1e451c77f91094f1b8be7a
Christopher Callan <Christopher.Robert.Callan@ibm.com>
2019-04-05 14:14:44 -0500

    Google Sheet form connection implemented and proper social media links

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  today = new Date();
+  timestamp = '';
+  showMsg: boolean = false;
+  
+  constructor() { 
+    this.timestamp = formatDate(this.today, 'MM-dd-yyyy hh:mm:ss a', 'en-US', '+4151−08739');
+  }
 
   ngOnInit() {
-  }
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyc5YNVj19yQ7EufCt8OnpAtFdyooqmKnp_yV7Mt7tOD2Q5yfxe/exec'
+    const form = document.forms['club-Tennis-Tryout-Form']
+
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+       .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+        this.showMsg= true;
+    })
+  }  
 
 }
